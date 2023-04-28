2204b8b0d2be0bfeb091517d297db538b741b5dc
AndreaCollura <andrea.collur@gmail.com>
2023-04-28 17:00:28 +0200

    minor fix 2

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,6 @@
 <script>
 
 import { store } from './data/store'
-import prova from './components/prova.vue'
 import HeaderComp from './components/HeaderComp.vue';
 import MainComp from './components/MainComp.vue';
 import FooterComp from './components/FooterComp.vue';
